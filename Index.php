6c0d845186b68353d60aa050e1e673f24bdebaca
johny2k <lee@johny2k.com>
2021-07-20 11:27:42 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -7,6 +7,16 @@
     <title>A Simple PHP Calculator</title>
   </head>
   <body>
-    
+    <h2>A simple php calculator</h2>
+    <form action="functions.php" method="get">
+      <input type="text" name="number01" plaeholder="Insert number here">
+      <select name="opt">
+        <label>Select here</label>
+        <option value="add">Add</option>
+        <option value="sub">Substract</option>
+      </select>
+      <input type="text" name="number02" placeholder="Insert number here">
+      <button type="submit">Calculate!</button>
+    </form>
   </body>
 </html>
