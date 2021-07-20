b05939196db31a1a74ac78fe946e8510e03ec635
johny2k <lee@johny2k.com>
2021-07-20 11:16:43 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,12 @@
 <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-sclae=1.0">
+    <link rel="stylesheet" href="css/custom.css">
+    <title>A Simple PHP Calculator</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
